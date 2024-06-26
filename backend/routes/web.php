ae8ba640a480a1716a52303a2e2e4f8004d77df2
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-06-26 16:46:09 -0400

    Ajouter route pour voitures et bien intégrer Inertia
    -Ajoute la route RESTful pour la gestion des voitures dans VoituresController
    -Faire afficher le composant react a partir du controlleur Voitures

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,14 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoituresController;
 
-// // function nouveautes dans le controller qui va retourner les voitures  récemment ajoutées
-// Route::get('/', [VoituresController::class, 'nouveautes'])->name('home');
-
-
-// Route::get('/voitures', [VoituresController::class, 'index'])->name('voitures.index');
-
-Route::get('/', function () {
-    return inertia('Home');
-});
+Route::get('/', [VoituresController::class, 'index'])->name('Accueil'); 
+Route::resource('/voitures', VoituresController::class);
