23b41fd95ea9af0ccdd63a20fb6985de0385c302
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-06-27 19:09:34 -0400

    Ajouter fillable et infos dans le Model Utilisateur

--- backend/app/Models/Utilisateur.php
+++ backend/app/Models/Utilisateur.php
@@ -7,8 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Utilisateur extends Model
 {
-
     use HasFactory;
 
-}
+    protected $table = 'utilisateurs';
+    protected $primaryKey = 'id_utilisateur';
+    public $timestamps = false;
 
+    protected $fillable = [
+        'prenom', 'nom', 'date_naissance', 'adresse', 'code_postal', 
+        'numero_telephone', 'numero_portable', 'courriel', 
+        'privileges_id', 'nom_utilisateur', 'mot_de_passe', 
+        'derniere_connexion', 'villes_id_ville'
+    ];
+
+   
+}
