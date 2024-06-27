70f1b68dd8f9c62c167d071cc7abf1d4824520a0
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-06-27 10:37:48 -0400

    Ajouter route pour Utilisateurcontroller

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoituresController;
+use App\Http\Controllers\UtilisateursController;
+
 
 Route::get('/', [VoituresController::class, 'index'])->name('Accueil'); 
 Route::resource('/voitures', VoituresController::class);
+Route::resource('/utilisateurs', UtilisateursController::class);
