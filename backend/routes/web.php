63cc80a5c1256179215397f1754bb7b4fea61e8f
Tiago Barros <tiagolp@msn.com>
2024-06-27 11:03:00 -0400

    Ajouter Moldels et contrôleur utilisateur

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoituresController;
-use App\Http\Controllers\UtilisateursController;
+use App\Http\Controllers\UtilisateurController;
 
 
-Route::get('/', [VoituresController::class, 'index'])->name('Accueil'); 
+Route::get('/', [VoituresController::class, 'index'])->name('Accueil');
 Route::resource('/voitures', VoituresController::class);
-Route::resource('/utilisateurs', UtilisateursController::class);
+Route::resource('/utilisateurs', UtilisateurController::class);
