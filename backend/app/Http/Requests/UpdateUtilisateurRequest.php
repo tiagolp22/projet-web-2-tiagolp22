9da98c554cf77b1e8c14456aa9984eaa10db45d5
Tiago Barros <tiagolp@msn.com>
2024-06-30 22:03:47 -0400

    Configurer user tests unitaires
    - Ajouté le fichier .env.testing pour configurer l'environnement de test
    - Assuré que les migrations de la base de données sont exécutées pour l'environnement de test
    - Mis à jour UtilisateurControllerTest pour utiliser correctement RefreshDatabase
    - Corrigé les assertions de la base de données dans les tests unitaires pour éviter les problèmes de table existante
    - Amélioration des assertions de test pour une meilleure fiabilité

---
+++ backend/app/Http/Requests/UpdateUtilisateurRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUtilisateurRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'prenom' => 'required|string|max:50',
+            'nom' => 'required|string|max:50',
+            'date_naissance' => 'required|date',
+            'adresse' => 'required|string|max:255',
+            'code_postal' => 'required|string|max:10',
+            'numero_telephone' => 'required|string|max:20',
+            'numero_portable' => 'nullable|string|max:20',
+            'courriel' => 'required|string|email|max:100|unique:utilisateurs,courriel,' . $this->route('utilisateur'),
+            'privileges_id' => 'required|integer|exists:privileges,id_privilege',
+            'nom_utilisateur' => 'required|string|max:50|unique:utilisateurs,nom_utilisateur,' . $this->route('utilisateur'),
+            'mot_de_passe' => 'nullable|string|min:8',
+            'villes_id_ville' => 'required|integer|exists:villes,id_ville'
+        ];
+    }
+}
