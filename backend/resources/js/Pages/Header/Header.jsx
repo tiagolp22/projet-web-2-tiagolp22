11199ed3b53869fb36ea83a7afe91f434a747ee3
Tiago Barros <tiagolp@msn.com>
2024-07-03 06:23:30 -0400

    fix: correction des erreurs et traduction - Correction d'une erreur CSS dans le fichier Header.css. - Ajout du support de traduction en utilisant react-i18next dans les composants Header, Footer et LoginModal. - Mise à jour des fichiers de traduction en.json et fr.json pour inclure les nouvelles clés.

--- backend/resources/js/Pages/Header/Header.jsx
+++ backend/resources/js/Pages/Header/Header.jsx
@@ -19,49 +19,46 @@ function Header() {
 
   return (
     <header className="custom-header">
-    <div className="wrapper">
-      <div className="header-content">
-        <div className="logo-section">
-          <Link href="/" className="logo">
-            <img className="logo" src="../../img/logo/logo.png" alt="Logo QuebeCar" />
-          </Link>
-        </div>
-        <div className="nav-section">
-          <nav className="nav-links">
-            <Link href="/" className="nav-link">{t('header.home')}</Link>
-            <Link href="/voitures" className="nav-link">{t('header.catalogue')}</Link>
-            <Link href="/about" className="nav-link">{t('header.about')}</Link>
-            <Link href="/contact" className="nav-link">{t('header.contact')}</Link>
+      <div className="wrapper">
+        <div className="header-content">
+          <div className="logo-section">
+            <Link href="/" className="logo">
+              <img className="logo" src="../../img/logo/logo.png" alt="Logo QuebeCar" />
+            </Link>
+          </div>
+          <div className="nav-section">
+            <nav className="nav-links">
+              <Link href="/" className="nav-link">{t('header.home')}</Link>
+              <Link href="/voitures" className="nav-link">{t('header.catalogue')}</Link>
+              <Link href="/about" className="nav-link">{t('header.about')}</Link>
+              <Link href="/contact" className="nav-link">{t('header.contact')}</Link>
+            </nav>
             <div className="icones">
               <a href="https://www.google.ca/maps/place/Coll%C3%A8ge+de+Maisonneuve/@45.5508613,-73.5558744,16z/data=!3m1!4b1!4m6!3m5!1s0x4cc91bf5bacbeffd:0x68ff300997eff5c!8m2!3d45.5508613!4d-73.5532995!16zL20vMDU3dHFx?hl=fr&entry=ttu" target="_blank" rel="noopener noreferrer">
-                <img className="icones_navigation" src="../../img/icones/Location.png" alt="" />
+                <img className="icones_navigation" src="../../img/icones/Location.png" alt="Location" />
               </a>
               <div className="dropdown">
                 <button className="dropdown-button">
-                  <img className="icones_navigation" src="../../img/icones/Profil.png" alt="" />
+                  <img className="icones_navigation" src="../../img/icones/Profil.png" alt="Profile" />
                 </button>
                 <div className="dropdown-content">
-                  <button onClick={openModal} className="dropdown-button nav-link">{t('header.login')}</button> 
+                  <button onClick={openModal} className="dropdown-button nav-link">{t('header.login')}</button>
                   <Link href="/register" className="nav-link">{t('header.register')}</Link>
                 </div>
               </div>
               <button className="language-button" onClick={() => changeLanguage('en')} aria-label="English">
-                <img className="icones_navigation" src={i18n.language === 'en' ? "../../img/icones/Canada_active.png" : "../../img/icones/Canada.png"} alt="" />
+                <img className="icones_navigation" src={i18n.language === 'en' ? "../../img/icones/Canada.png" : "../../img/icones/Canada.png"} alt="English" />
               </button>
               <button className="language-button" onClick={() => changeLanguage('fr')} aria-label="Français">
-                <img className="icones_navigation" src={i18n.language === 'fr' ? "../../img/icones/France_active.png" : "../../img/icones/France.png"} alt="" />
+                <img className="icones_navigation" src={i18n.language === 'fr' ? "../../img/icones/France.png" : "../../img/icones/France.png"} alt="Français" />
               </button>
-
             </div>
-            <Link href="/login">
-              <img className="icones_navigation" src="../../img/icones/Canada.png" alt="Drapeua do Canada" />
-            </Link>
+
           </div>
         </div>
       </div>
       {showModal && <LoginModal onClose={() => setShowModal(false)} />} {/* Affiche le modal si showModal est true */}
     </header>
-
   );
 }
 
